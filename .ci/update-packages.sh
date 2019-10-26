85f0cce5038a9f4322a7b5d14a0403c74e1be4e1
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-10-26 20:55:18 +0200

    Fix update packages script

--- .ci/update-packages.sh
+++ .ci/update-packages.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
 ROOT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )
 
 DIRECTORIES=(
@@ -17,12 +16,18 @@ echo "Updating dependencies..."
 
 function updateDependencies() {
     echo "> $1"
+    cd $2
     npm outdated
     npm update
+    npm audit fix
 }
 
 for directory in "${DIRECTORIES[@]}"
    do
      CURRENT_PATH=${ROOT_PATH}/${directory}
-     updateDependencies ${directory}
+     updateDependencies ${directory} ${CURRENT_PATH}
    done
+
+echo "Bringing back symlinks..."
+cd ${ROOT_PATH}
+npm run bootstrap
